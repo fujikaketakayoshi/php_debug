7b848e22a78415dada908f9af1c60f9b0f052a84
fjkk <fujikake.takayoshi@gmail.com>
2021-04-24 23:17:12 +0900

    APPPATH added

--- helper/debug_helper.php
+++ helper/debug_helper.php
@@ -5,6 +5,6 @@
 
 if ( ! function_exists('var_log')) {
     function var_log($var) {
-        error_log(var_export($var, true) . "\n", 3, $_SERVER['CONTEXT_DOCUMENT_ROOT'] . '/application/logs/debug.log');
+        error_log(var_export($var, true) . "\n", 3, APPPATH . '/logs/debug.log');
     }
 }
